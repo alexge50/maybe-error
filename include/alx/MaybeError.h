6a04d7b9982c97178c59a827edb2821bdf22b3f9
alexge50 <alexge50.x@gmail.com>
2019-08-05 11:55:57 +0300

    implemented discard() to terminate and error out if errorSet()

--- include/alx/MaybeError.h
+++ include/alx/MaybeError.h
@@ -8,13 +8,37 @@
 #include <optional>
 #include <utility>
 #include <cassert>
+#include <iostream>
 
 namespace alx
 {
 
+    namespace detail {
+        template<typename, template<typename...> typename Pattern, typename... Args>
+        struct IllFormed {
+            constexpr static bool value = false;
+        };
+
+        template<typename... Ts>
+        using void_t = void;
+
+        template<template<typename...> typename Pattern, typename... Args>
+        struct IllFormed<void_t<Pattern<Args...>>, Pattern, Args...> {
+            constexpr static bool value = true;
+        };
+
+        template<template<typename...> typename Pattern, typename... Args>
+        struct Detect : public detail::IllFormed<void, Pattern, Args...> {
+        };
+
+    }
+
 template<typename ErrorType>
 class [[nodiscard]] MaybeError
 {
+    template<typename T>
+    using Printable_d = decltype(std::cerr << std::declval<T>());
+
 public:
     MaybeError(): m_error{std::nullopt} {}
     MaybeError(ErrorType error): m_error{std::move(error)} {}
@@ -42,7 +66,16 @@ public:
             handler(m_error);
     }
 
-    void discard() { }
+    void discard()
+    {
+        if (errorSet())
+        {
+            if constexpr(detail::Detect<Printable_d>::value)
+                std::cerr << "Discarded MaybeError with error set: \n" << m_error.value() << std::endl;
+            else std::cerr << "Discarded MaybeError with error set" << std::endl;
+            std::terminate();
+        }
+    }
 
 private:
     std::optional<ErrorType> m_error;
